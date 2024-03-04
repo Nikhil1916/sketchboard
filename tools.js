18a20edd256c668aff133493efa342b9d9994b48
nikhil <nikhilchawla9013@gmail.com>
2024-03-04 21:21:32 +0530

    drag and drop started

--- tools.js
+++ tools.js
@@ -7,6 +7,7 @@ let isPencilFlag = false;
 let isEraserFlag = false;
 const pencilImg = document.querySelector(".pencil");
 const eraseImg = document.querySelector(".erase");
+const sticky = document.querySelector(".sticky");
 
 
 // isOptionsFlag true -> show tools , false =>hide tools
@@ -49,3 +50,54 @@ eraseImg.addEventListener("click",e=>{
         eraserToolCont.style.display = "flex";
     } else eraserToolCont.style.display = "none";
 })
+
+sticky.addEventListener("click", (e) => {
+  const stickyCont = document.createElement("div");
+  stickyCont.setAttribute("class", "sticky-cont");
+  stickyCont.innerHTML = `
+    <div class="header-cont">
+    <div class="minimize"></div>
+    <div class="remove"></div>
+</div>
+<div class="note-cont">
+    <textarea></textarea>
+</div>
+    `;
+    document.body.appendChild(stickyCont);
+    stickyCont.onmousedown = function(event) {
+        dragAndDrop(stickyCont, )
+      };
+});
+
+function dragAndDrop(element) {
+// (1) prepare to moving: make absolute and on top by z-index
+ball.style.position = 'absolute';
+ball.style.zIndex = 1000;
+
+// move it out of any current parents directly into body
+// to make it positioned relative to the body
+document.body.append(ball);
+
+// centers the ball at (pageX, pageY) coordinates
+function moveAt(pageX, pageY) {
+  ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
+  ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+}
+
+// move our absolutely positioned ball under the pointer
+moveAt(event.pageX, event.pageY);
+
+function onMouseMove(event) {
+  moveAt(event.pageX, event.pageY);
+}
+
+// (2) move the ball on mousemove
+document.addEventListener('mousemove', onMouseMove);
+
+// (3) drop the ball, remove unneeded handlers
+ball.onmouseup = function() {
+  document.removeEventListener('mousemove', onMouseMove);
+  ball.onmouseup = null;
+};
+
+}
