c15323b1005a8bbf12f29ade5034032789127f84
nikhil <nikhilchawla9013@gmail.com>
2024-03-04 22:43:36 +0530

    drag and drop added on sticky notes

--- tools.js
+++ tools.js
@@ -9,47 +9,45 @@ const pencilImg = document.querySelector(".pencil");
 const eraseImg = document.querySelector(".erase");
 const sticky = document.querySelector(".sticky");
 
-
 // isOptionsFlag true -> show tools , false =>hide tools
 optionCont.addEventListener("click", (e) => {
-    isOptionsFlag = !isOptionsFlag;
-    if(isOptionsFlag) {
-        openTools();
-    } else {
-        closeTools();
-    }
+  isOptionsFlag = !isOptionsFlag;
+  if (isOptionsFlag) {
+    openTools();
+  } else {
+    closeTools();
+  }
 });
 
 function openTools() {
-    const iconEl = optionCont?.children[0];
-    iconEl.classList.remove("fa-times");
-    iconEl.classList.add("fa-bars");
-    toolsCont.style.display = "flex";
+  const iconEl = optionCont?.children[0];
+  iconEl.classList.remove("fa-times");
+  iconEl.classList.add("fa-bars");
+  toolsCont.style.display = "flex";
 }
 
 function closeTools() {
-    const iconEl = optionCont?.children[0];
-    iconEl.classList.add("fa-times");
-    iconEl.classList.remove("fa-bars");
-    toolsCont.style.display = "none";
-    pencilToolsCont.style.display = "none";
-    eraserToolCont.style.display = "none"
+  const iconEl = optionCont?.children[0];
+  iconEl.classList.add("fa-times");
+  iconEl.classList.remove("fa-bars");
+  toolsCont.style.display = "none";
+  pencilToolsCont.style.display = "none";
+  eraserToolCont.style.display = "none";
 }
 
-pencilImg.addEventListener("click",e=>{
-    isPencilFlag = !isPencilFlag;
-    if(isPencilFlag) {
-        pencilToolsCont.style.display = "block";
-    } else pencilToolsCont.style.display = "none";
+pencilImg.addEventListener("click", (e) => {
+  isPencilFlag = !isPencilFlag;
+  if (isPencilFlag) {
+    pencilToolsCont.style.display = "block";
+  } else pencilToolsCont.style.display = "none";
 });
 
-
-eraseImg.addEventListener("click",e=>{
-    isEraserFlag = !isEraserFlag;
-    if(isEraserFlag) {
-        eraserToolCont.style.display = "flex";
-    } else eraserToolCont.style.display = "none";
-})
+eraseImg.addEventListener("click", (e) => {
+  isEraserFlag = !isEraserFlag;
+  if (isEraserFlag) {
+    eraserToolCont.style.display = "flex";
+  } else eraserToolCont.style.display = "none";
+});
 
 sticky.addEventListener("click", (e) => {
   const stickyCont = document.createElement("div");
@@ -63,41 +61,43 @@ sticky.addEventListener("click", (e) => {
     <textarea></textarea>
 </div>
     `;
-    document.body.appendChild(stickyCont);
-    stickyCont.onmousedown = function(event) {
-        dragAndDrop(stickyCont, )
-      };
+  document.body.appendChild(stickyCont);
+  stickyCont.onmousedown = function (event) {
+    dragAndDrop(stickyCont, event);
+  };
+
+  stickyCont.ondragstart = function () {
+    return false;
+  };
 });
 
-function dragAndDrop(element) {
-// (1) prepare to moving: make absolute and on top by z-index
-ball.style.position = 'absolute';
-ball.style.zIndex = 1000;
+function dragAndDrop(element, event) {
+  let shiftX = event.clientX - element.getBoundingClientRect().left;
+  let shiftY = event.clientY - element.getBoundingClientRect().top;
 
-// move it out of any current parents directly into body
-// to make it positioned relative to the body
-document.body.append(ball);
+  element.style.position = "absolute";
+  element.style.zIndex = 1000;
+  document.body.append(element);
 
-// centers the ball at (pageX, pageY) coordinates
-function moveAt(pageX, pageY) {
-  ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
-  ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
-}
-
-// move our absolutely positioned ball under the pointer
-moveAt(event.pageX, event.pageY);
-
-function onMouseMove(event) {
   moveAt(event.pageX, event.pageY);
-}
 
-// (2) move the ball on mousemove
-document.addEventListener('mousemove', onMouseMove);
+  // moves the element at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    element.style.left = pageX - shiftX + "px";
+    element.style.top = pageY - shiftY + "px";
+  }
 
-// (3) drop the ball, remove unneeded handlers
-ball.onmouseup = function() {
-  document.removeEventListener('mousemove', onMouseMove);
-  ball.onmouseup = null;
-};
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
 
+  // move the element on mousemove
+  document.addEventListener("mousemove", onMouseMove);
+
+  // drop the element, remove unneeded handlers
+  element.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    element.onmouseup = null;
+  };
 }
